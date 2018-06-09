27e3eaf49f48a011adaaf384a254888751e27612
afayelaz <afayelaz@gmail.com>
2018-06-09 16:19:43 -0500

    etsycomlink

---
+++ src/com/cbt/googleSearch.java
@@ -0,0 +1,19 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class googleSearch {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", 
+				"/Users/alacaf/Documents/selenium dependencies/drivers/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.etsy.com");
+
+	}
+
+}
